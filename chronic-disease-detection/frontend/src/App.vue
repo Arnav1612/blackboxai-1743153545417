d6f0e3fe410eb7e1d9ee610ec253873b39bb6345
Arnav1612 <arnavsingh1612@gmail.com>
2025-03-28 14:49:27 +0530

    Add chronic-disease-detection/frontend/src/App.vue 1743153566925

---
+++ chronic-disease-detection/frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <router-view />
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+@import './styles/tailwind.css';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
